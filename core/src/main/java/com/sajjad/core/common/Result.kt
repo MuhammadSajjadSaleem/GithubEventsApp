c6a5ff7b8f19a4efd605e725266be953195e5528
muh.sajjadsaleem@gmail.com <muh.sajjadsaleem@gmail.com>
2025-09-03 08:48:14 +0200

    setup network classes.

--- core/src/main/java/com/sajjad/core/common/Result.kt
+++ core/src/main/java/com/sajjad/core/common/Result.kt
@@ -3,5 +3,5 @@ package com.sajjad.core.common
 sealed class Result<out T> {
     data class Success<T>(val data: T) : Result<T>()
     data class Error(val exception: Throwable) : Result<Nothing>()
-    object Loading : Result<Nothing>()
+    data object Loading : Result<Nothing>()
 }
--- core/src/main/java/com/sajjad/core/network/NetworkModule.kt
+++ core/src/main/java/com/sajjad/core/network/NetworkModule.kt
@@ -1,6 +1,5 @@
 package com.sajjad.core.network
 
-import retrofit2.Retrofit
 import com.squareup.moshi.Moshi
 import dagger.Module
 import dagger.Provides
@@ -8,21 +7,41 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
-
+import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+
     @Provides
     fun provideBaseUrl(): String = "https://api.github.com/"
 
     @Provides
-    fun provideRetrofit(baseUrl: String): Retrofit = Retrofit.Builder()
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        val logging = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        return OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        baseUrl: String,
+        okHttpClient: OkHttpClient,
+        moshi: Moshi
+    ): Retrofit = Retrofit.Builder()
         .baseUrl(baseUrl)
-        .addConverterFactory(MoshiConverterFactory.create())
+        .client(okHttpClient)
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
         .build()
 
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi = Moshi.Builder().build()
 }
-
-
